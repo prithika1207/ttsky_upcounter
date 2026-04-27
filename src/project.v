ee16b42237d35c589f8b58c8ff3ad11796ce9c13
prithika1207 <prithikapradhaban0@gmail.com>
2026-04-27 11:59:17 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,14 +1,17 @@
 module tt_um_upcounter (
     input  wire clk,
     input  wire rst_n,
+    input  wire ena,
     output reg  [3:0] out
 );
 
-always @(posedge clk or negedge rst_n) begin
+wire enable = ena;  // you can also ignore or force it
+
+always @(posedge clk) begin
     if (!rst_n)
         out <= 4'b0000;
-    else
-        out <= out + 4'b0001;
+    else if (enable)
+        out <= out + 1'b1;
 end
 
 endmodule
